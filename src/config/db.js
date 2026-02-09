3e76c1ab452a237295dc1be362f7dbcce6fc9b18
prasadd90 <prasadd90@gmail.com>
2026-02-09 23:19:59 +0530

    initial commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/MyDB";
-    await mongoose.connect(mongoURI);
-
+    const mongoURI = "mongodb+srv://deshmukhprasad573_db_user:Oo6X5pKKXtXLLZAp@cluster0.critjdp.mongodb.net";
+    //mongodb://localhost:27017/MyDB
+    //await mongoose.connect(mongoURI);
+await mongoose.connect(process.env.MONGO_URI || mongoURI)
     if (mongoose.connection.readyState === 1) {
       console.log("MongoDB is connected - Ready State: " + mongoose.connection.readyState);
+       console.log("MongoDB is connected - Ready State: " + process.env.MONGO_URI);
     } else {
       console.log("MongoDB is NOT connected");
     }
